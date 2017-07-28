babac9b288c247b87ca59ead1247c919b3d46375
alucas- <alucas-@student.42.fr>
2017-07-28 08:43:54 +0200

    added d02

---
+++ d02/ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alucas- <alucas-@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/05 07:52:09 by alucas-           #+#    #+#             */
+/*   Updated: 2017/07/05 07:57:24 by alucas-          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_print_reverse_alphabet(void)
+{
+	char letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		ft_putchar(letter--);
+	}
+}
