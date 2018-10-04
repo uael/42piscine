df475027d3c6c730d7f3c1b943538d572a1fdd1f
uael <lucas.abel@yandex.com>
2018-10-04 17:20:58 +0200

    [ocaml] d02/ex02 (end)

--- OCaml/d02/ex02/gray.ml
+++ OCaml/d02/ex02/gray.ml
@@ -1,9 +1,17 @@
 let gray n =
-  let mv i =
-    i lxor (i lsr 1)
-  in let rec toa a i j =
-    if j = n then a else
-    if i land (1 lsl j) then a ^ "1" ^ toa a i (j + 1)
-    else                     a ^ "0" ^ toa a i (j + 1)
-  in 
-      
+  if n < 0 then () else
+  let rec print i j =
+    print_char (if i land (1 lsl j) != 0 then '1' else '0');
+    if j > 0 then print i (j - 1)
+  in let rec lul i =
+    print (i lxor (i lsr 1)) (n - 1);
+    if i < (1 lsl n) - 1 then begin
+      print_char ' '; lul (i + 1)
+    end
+  in lul 0; print_char '\n'
+
+let () =
+  gray 0;
+  gray 1;
+  gray 2;
+  gray 3
