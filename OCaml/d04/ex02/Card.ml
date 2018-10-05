3a90010daee9212051faefb70023f068bee62344
uael <lucas.abel@yandex.com>
2018-10-05 20:19:18 +0200

    [ocaml] d04/ex02

--- OCaml/d04/ex02/Card.ml
+++ OCaml/d04/ex02/Card.ml
@@ -1,4 +1,4 @@
-module Color : sig
+module Color = struct
   type t = Spade | Heart | Diamond | Club
 
   let all = [Spade; Heart; Diamond; Club]
@@ -16,7 +16,7 @@ module Color : sig
   | Club -> "Club"
 end
 
-module Value : sig
+module Value = struct
   type t = T2 | T3 | T4 | T5 | T6 | T7 | T8 | T9 | T10 | Jack | Queen | King
     | As
 
@@ -102,10 +102,10 @@ type t = (Value.t * Color.t)
 
 let newCard v c = (v, c)
 
-let allSpades   = List.map (fun v -> newCard c Color.Spade)   Value.all
-let allHearts   = List.map (fun v -> newCard c Color.Hearth)  Value.all
-let allDiamonds = List.map (fun v -> newCard c Color.Diamond) Value.all
-let allClubs    = List.map (fun v -> newCard c Color.Club)    Value.all
+let allSpades   = List.map (fun v -> newCard v Color.Spade)   Value.all
+let allHearts   = List.map (fun v -> newCard v Color.Heart)   Value.all
+let allDiamonds = List.map (fun v -> newCard v Color.Diamond) Value.all
+let allClubs    = List.map (fun v -> newCard v Color.Club)    Value.all
 let all         = allSpades @ allHearts @ allDiamonds @ allClubs
 
 let getValue = function | (v, _) -> v
@@ -118,7 +118,35 @@ let toStringVerbose = function
 | (v, c) -> "Card(" ^ (Value.toStringVerbose v) ^ ", " ^
   (Color.toStringVerbose c) ^ ")"
 
-let compare = function
-| (va, _) (vb, _) -> Value.toInt va <> Value.toInt vb
+let compare (v1, _) (v2, _) =
+  match (Value.toInt v1) - (Value.toInt v2) with
+  |	c when c > 0 -> 1
+  | c when c < 0 -> -1
+  | _ -> 0
 
+let max a b = match compare a b with
+| 0 | 1 -> a
+| -1 -> b
 
+let min a b = match compare a b with
+| 0 | -1 -> a
+| 1 -> b
+
+let best = function
+| [] -> invalid_arg "invalid operation, you idiot"
+| h::t -> List.fold_left max h t
+
+let isOf (_, co1) co2 =
+  co1 = co2
+
+let isSpade ca =
+  isOf ca Color.Spade
+
+let isHeart ca =
+  isOf ca Color.Heart
+
+let isDiamond ca =
+  isOf ca Color.Diamond
+
+let isClub ca =
+  isOf ca Color.Club
--- OCaml/d04/ex02/main.ml
+++ OCaml/d04/ex02/main.ml
@@ -1,1 +1,31 @@
 let () =
+  print_endline "clubs:";
+  List.iter (fun c -> print_endline (Card.toString c)) Card.allClubs;
+  List.iter (fun c -> print_endline (Card.toStringVerbose c)) Card.allClubs;
+  print_endline "diamonds:";
+  List.iter (fun c -> print_endline (Card.toString c)) Card.allDiamonds;
+  List.iter (fun c -> print_endline (Card.toStringVerbose c)) Card.allDiamonds;
+  print_endline "hearths:";
+  List.iter (fun c -> print_endline (Card.toString c)) Card.allHearts;
+  List.iter (fun c -> print_endline (Card.toStringVerbose c)) Card.allHearts;
+  print_endline "spades:";
+  List.iter (fun c -> print_endline (Card.toString c)) Card.allSpades;
+  List.iter (fun c -> print_endline (Card.toStringVerbose c)) Card.allSpades;
+  print_endline "all:";
+  List.iter (fun c -> print_endline (Card.toString c)) Card.all;
+  List.iter (fun c -> print_endline (Card.toStringVerbose c)) Card.all;
+  let c = Card.newCard Card.Value.Jack Card.Color.Spade in begin
+    print_endline ("with card: " ^ Card.toStringVerbose c);
+    print_endline ("value: " ^ Card.Value.toStringVerbose (Card.getValue c));
+    print_endline ("color: " ^ Card.Color.toStringVerbose (Card.getColor c));
+    print_endline ("is of Spade: " ^ string_of_bool (Card.isOf c Card.Color.Spade));
+    print_endline ("is of Diamond: " ^ string_of_bool (Card.isOf c Card.Color.Diamond));
+    print_endline ("is Spade: " ^ string_of_bool (Card.isSpade c));
+    print_endline ("is Diamond: " ^ string_of_bool (Card.isDiamond c));
+    let c2 = Card.newCard Card.Value.As Card.Color.Club in begin
+      print_endline ("max: " ^ Card.toStringVerbose (Card.max c c2));
+      print_endline ("min: " ^ Card.toStringVerbose (Card.min c c2))
+    end
+  end;
+  let c = Card.best Card.allHearts in
+    print_endline ("best in allHearts: " ^ Card.toStringVerbose c)
