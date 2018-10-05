f3c23ebd2a6b4ed2585cbd2063dfe5782fb5afd9
uael <lucas.abel@yandex.com>
2018-10-05 11:38:47 +0200

    [ocaml] d03/ex01 (cleanup 2)

--- OCaml/d03/ex01/gardening.ml
+++ OCaml/d03/ex01/gardening.ml
@@ -7,34 +7,34 @@ let rec size = function
 let rec height = function
 | Nil -> 0
 | Node(_, l, r) ->
-	1 + let ls = size l in let lr = size r in if ls > lr then ls else lr
+  1 + let ls = size l in let lr = size r in if ls > lr then ls else lr
 
 let draw_tree t =
-	let ss = 40 in let sw = ss / 2 in let sh = ss / 4 in
-	let rec draw_node x y n =
-		match n with
-		| Nil -> ()
-		| Node(v, l, r) -> begin
-			let draw_child d = function
-			| Nil -> ()
-			| c -> begin
-				Graphics.moveto (x + sw) y; Graphics.lineto (x + (ss * 2) - sw) (y + d);
+  let ss = 40 in let sw = ss / 2 in let sh = ss / 4 in
+  let rec draw_node x y n =
+    match n with
+    | Nil -> ()
+    | Node(v, l, r) -> begin
+      let draw_child d = function
+      | Nil -> ()
+      | c -> begin
+        Graphics.moveto (x + sw) y; Graphics.lineto (x + (ss * 2) - sw) (y + d);
         draw_node (x + (ss * 2)) (y + d) c
-			end
-			in begin
-				Graphics.moveto (x - sw) (y - sh);
+      end
+      in begin
+        Graphics.moveto (x - sw) (y - sh);
         Graphics.lineto (x - sw) (y + sh); Graphics.lineto (x + sw) (y + sh);
         Graphics.lineto (x + sw) (y - sh); Graphics.lineto (x - sw) (y - sh);
         Graphics.moveto (x - sw + 5) (y - 5); Graphics.draw_string v;
-				draw_child ((sh * (height l)) *  1) l;
-				draw_child ((sh * (height r)) * -1) r;
-			end
-		end
-	in draw_node 30 (((height t) * sh) + 30) t
+        draw_child ((sh * (height l)) *  1) l;
+        draw_child ((sh * (height r)) * -1) r;
+      end
+    end
+  in draw_node 30 (((height t) * sh) + 30) t
 
 let () =
-	Graphics.open_graph "";
-	let t = Node("root",
+  Graphics.open_graph "";
+  let t = Node("root",
     Node("l",
       Node("ll", Nil, Node("llr", Nil, Nil)),
       Node("lr", Node("rll", Nil, Nil), Nil)
@@ -44,4 +44,4 @@ let () =
       Node("rr", Nil, Node("rrr", Nil, Nil))
     )
   ) in draw_tree t;
-	ignore (Graphics.read_key ())
+  ignore (Graphics.read_key ())
