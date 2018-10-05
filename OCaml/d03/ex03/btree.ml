76f77704ce74ed05f4678cde0434ff338baa31b4
uael <lucas.abel@yandex.com>
2018-10-05 16:03:17 +0200

    [ocaml] d03/ex03

--- OCaml/d03/ex03/btree.ml
+++ OCaml/d03/ex03/btree.ml
@@ -6,16 +6,44 @@ let rec height = function
   1 + let ls = height l in let lr = height r in if ls > lr then ls else lr
 
 let rec is_bst = function
-  | Nil -> true
-  | Node(i, l, r) ->
-    (is_bst l && (match l with
-    | Nil -> true
-    | Node(j, _, _) -> j < i)) &&
-    (is_bst r && (match r with
-    | Nil -> true
-    | Node(j, _, _) -> j > i))
+| Nil -> true
+| Node(i, l, r) ->
+  (is_bst l && match l with | Nil -> true | Node(j, _, _) -> j < i) &&
+  (is_bst r && match r with | Nil -> true | Node(j, _, _) -> j > i)
 
 let rec is_perfect = function
-  | Nil -> true | Node(i, Nil, Nil) -> true
-  | Node(i, Nil, _) -> false | Node(i, _, Nil) -> false
-  | Node(i, l, r) -> (is_perfect l) && (is_perfect r) && (height l) = (height r)
+| Nil | Node(_, Nil, Nil) -> true
+| Node(_, Nil, _) | Node(_, _, Nil) -> false
+| Node(i, l, r) -> is_perfect l && is_perfect r &&
+  height l = height r
+
+let rec is_balanced = function
+| Nil -> true
+| Node(i, l, r) -> is_balanced l && is_balanced r &&
+  abs (height l - height r) <= 1
+
+let rec search_bst v = function
+| Nil -> false
+| Node(i, l, r) when v = i -> true
+| Node(i, l, _) when v < i -> search_bst v l
+| Node(i, _, r) -> search_bst v r
+
+let rec add_bst v = function
+| Nil -> Node(v, Nil, Nil)
+| Node(i, l, r) when v = i -> failwith "invalid operation, you idiot"
+| Node(i, l, r) when v < i -> Node(i, add_bst v l, r)
+| Node(i, l, r) -> Node(i, l, add_bst v r)
+
+let rec max_bst = function
+| Nil -> failwith "invalid operation, you idiot"
+| Node (x, _, Nil) -> x
+| Node (_, _, r) -> max_bst r
+
+let rec delete_bst v = function
+| Nil -> failwith "invalid operation, you idiot"
+| Node(i, Nil, Nil) when v = i -> Nil
+| Node(i,   l, Nil) when v = i -> l
+| Node(i, Nil,   r) when v = i -> r
+| Node(i,   l,   r) when v = i -> Node(max_bst l, delete_bst v l, r)
+| Node(i,   l,   r) when v < i -> Node(i, delete_bst v l, r)
+| Node(i,   l,   r) -> Node(i, l, delete_bst v r)
--- OCaml/d03/ex03/main.ml
+++ OCaml/d03/ex03/main.ml
@@ -1,11 +1,90 @@
 open Btree;;
 
+let draw_tree t =
+  let ss = 40 in let sw = ss / 2 in let sh = ss / 4 in
+  let rec draw_node x y n =
+    match n with
+    | Nil -> ()
+    | Node(v, l, r) -> begin
+      let draw_child d = function
+      | Nil -> ()
+      | c -> begin
+        Graphics.moveto (x + sw) y; Graphics.lineto (x + (ss * 2) - sw) (y + d);
+        draw_node (x + (ss * 2)) (y + d) c
+      end
+      in begin
+        Graphics.moveto (x - sw) (y - sh);
+        Graphics.lineto (x - sw) (y + sh); Graphics.lineto (x + sw) (y + sh);
+        Graphics.lineto (x + sw) (y - sh); Graphics.lineto (x - sw) (y - sh);
+        Graphics.moveto (x - sw + 5) (y - 5);
+        Graphics.draw_string (string_of_int v);
+        draw_child ((sh * (height l)) *  1) l;
+        draw_child ((sh * (height r)) * -1) r;
+      end
+    end
+  in let r_height = function | Nil -> 0 | Node(_, _, r) -> height r
+  in draw_node 30 (((1 lsl ((r_height t) - 1)) * sw) + 5) t
+
 let () =
   let t = Node(4,
     Node(2, Node(1, Nil, Nil), Node(3, Nil, Nil)),
     Node(6, Node(5, Nil, Nil), Node(7, Nil, Nil))
   ) in begin
     print_endline (string_of_bool (is_bst t));
-    print_endline (string_of_bool (is_perfect t))
+    print_endline (string_of_bool (is_perfect t));
+  end;
+  let t = Node(4,
+    Node(2, Node(1, Nil, Nil), Node(0, Nil, Nil)),
+    Node(6, Node(5, Nil, Nil), Nil)
+  ) in begin
+    print_endline (string_of_bool (is_bst t));
+    print_endline (string_of_bool (is_perfect t));
+
+    print_endline (string_of_bool (search_bst 2 t));
+    print_endline (string_of_bool (search_bst 5 t));
+    print_endline (string_of_bool (search_bst 4 t));
+    print_endline (string_of_bool (search_bst 7 t));
+    print_endline (string_of_bool (search_bst 0 t));
+    print_endline (string_of_bool (search_bst 3 t));
+
+  end;
+  let t = Node(4,
+    Node(2, Node(1, Nil, Nil), Node(3, Nil, Nil)),
+    Node(6, Node(5, Nil, Nil), Node(7, Nil, Nil))
+  ) in begin
+    Graphics.open_graph ""; draw_tree t; ignore (Graphics.read_key ());
+
+    let t = delete_bst 1 t in (
+      Graphics.open_graph ""; draw_tree t;
+      ignore (Graphics.read_key ());
+      let t = delete_bst 7 t in (
+        Graphics.open_graph ""; draw_tree t;
+        ignore (Graphics.read_key ());
+        let t = delete_bst 3 t in (
+          Graphics.open_graph ""; draw_tree t;
+          ignore (Graphics.read_key ());
+          let t = delete_bst 5 t in (
+            Graphics.open_graph ""; draw_tree t;
+            ignore (Graphics.read_key ());
+            let t = add_bst 1 t in (
+              Graphics.open_graph ""; draw_tree t;
+              ignore (Graphics.read_key ());
+              let t = add_bst 7 t in (
+                Graphics.open_graph ""; draw_tree t;
+                ignore (Graphics.read_key ());
+                let t = add_bst 3 t in (
+                  Graphics.open_graph ""; draw_tree t;
+                  ignore (Graphics.read_key ());
+                  let t = add_bst 5 t in (
+                    Graphics.open_graph ""; draw_tree t;
+                    ignore (Graphics.read_key ())
+                  )
+                )
+              )
+            )
+          )
+        )
+      )
+    )
   end
 
