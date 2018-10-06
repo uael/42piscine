985593e8fa1c01624fcf43dc9066b9220f5b90b8
Brahim Hamidi <bhamidi@student.42.fr>
2018-10-06 15:58:07 +0200

    wip

--- OCaml/rush00/board.ml
+++ OCaml/rush00/board.ml
@@ -1,13 +1,13 @@
 type t = Player.m list
 
-let toggle r c m  = function
+let toggle (r, c) m  = function
   | [] -> failwith "error"
   | b ->
     if ((r > 2) || (r < 0)) || ((c > 2) && (c < 0))
     then failwith "Error value"
     else
       let i = c + (r * 3) in
-      let f index e = if index = i then e
+      let f index e = if index <> i then e
         else
           match e with
           | (X : Player.m) | O -> failwith "already in use"
@@ -16,8 +16,8 @@ let toggle r c m  = function
 
 let rec dump b =
   let f i a =
-    print_string (Printf.sprintf " %s " (Player.string_of_mark a)) ;
-    if (i mod 2) = 0 then print_char '\n'
+    print_string (Printf.sprintf "%s%c" (Player.string_of_mark a) (
+    if (((i+ 1) mod 3) = 0) && i <> 0 then '\n' else ' ' ))
   in List.iteri f b
 
 let make n : t =
@@ -25,3 +25,10 @@ let make n : t =
     if i < 0 then failwith "negatif value"
     else Player.N
   in List.init n f
+
+let () =
+  print_endline "** test Board module **\n";
+  let b = make 9 in
+  dump b ; print_char '\n' ;
+  let bplus = toggle (1, 2) Player.X b in 
+  dump bplus
