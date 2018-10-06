c2bd1a87782fece44572f15e86d301cb665290b8
Brahim Hamidi <bhamidi@student.42.fr>
2018-10-06 16:15:17 +0200

    add isTaken function

--- OCaml/rush00/board.ml
+++ OCaml/rush00/board.ml
@@ -1,23 +1,31 @@
 type t = Player.m list
 
-let toggle (r, c) m  = function
-  | [] -> failwith "error"
-  | b ->
-    if ((r > 2) || (r < 0)) || ((c > 2) && (c < 0))
-    then failwith "Error value"
-    else
-      let i = c + (r * 3) in
-      let f index e = if index <> i then e
-        else
-          match e with
-          | (X : Player.m) | O -> failwith "already in use"
-          | _ -> m in
-      List.mapi f b
+let toggle (r, c) m b =
+  let i = c + (r * 3) in
+  let f index e =
+    if index <> i then e
+    else m
+  in
+  List.mapi f b
+
+let isTaken (r, c) b =
+  let i = c + (r * 3) in
+  let f = function 
+    | (X : Player.m) | O -> true
+    | _ -> false
+  in
+  let rec loop l acc = match l with
+    | [] -> false
+    | hd :: tl ->
+      if acc = i then f hd
+      else loop tl (acc + 1)
+  in
+  loop b 0
 
 let rec dump b =
   let f i a =
     print_string (Printf.sprintf "%s%c" (Player.string_of_mark a) (
-    if (((i+ 1) mod 3) = 0) && i <> 0 then '\n' else ' ' ))
+        if (((i+ 1) mod 3) = 0) && i <> 0 then '\n' else ' ' ))
   in List.iteri f b
 
 let make n : t =
@@ -29,6 +37,8 @@ let make n : t =
 let () =
   print_endline "** test Board module **\n";
   let b = make 9 in
-  dump b ; print_char '\n' ;
+  dump b ; print_char '\n' ; print_endline "toggle (1, 2)" ;
   let bplus = toggle (1, 2) Player.X b in 
-  dump bplus
+  dump bplus ;
+  print_endline ("istaken (1, 2) -> " ^ (string_of_bool (isTaken (1, 2) bplus))) ;
+  print_endline ("istaken (2, 3) -> " ^ (string_of_bool (isTaken (2, 3) bplus))) ;
